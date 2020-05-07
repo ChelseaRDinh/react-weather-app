b9737ec193e08d586768baeccd2f7cc33707230b
chelseardinh <chelseadinh94@gmail.com>
2020-05-07 12:42:32 -0700

    Add dates

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -6,6 +6,22 @@ const api = {
 };
 
 function App() {
+
+  const dateBuilder = (d) => {
+	let months = ['January', 'February', 'March', 'April', 
+				'May', 'June', 'July', 'August', 'September',
+				'October', 'December'];
+
+	let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+	let day = days[d.getDay()];
+	let date = d.getDate();
+	let month = months[d.getMonth()];
+	let year = d.getFullYear();
+
+	return `${day} ${date} ${month} ${year}`;
+  };
+	
   return (
     <div className="app">
 		<main>
@@ -15,6 +31,12 @@ function App() {
 					className="search-bar" 
 					placeHolder="Search..."
 				/>
+			</div>
+			<div className="location-box">
+				<div className="location">
+					New York City, US
+				</div>
+				<div className="date">{dateBuilder(new Date())}</div>
 			</div>
 		</main>
     </div>
