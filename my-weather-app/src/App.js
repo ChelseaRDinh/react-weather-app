1e52977b059fd355314f8c80ca6cdb9771572251
chelseardinh <chelseadinh94@gmail.com>
2020-05-07 13:10:29 -0700

    Setup search box to have submission

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const api = {
 	key: '8441299d786f42839c775fe9179d963a',
-	base: 'https://api.openweathermap.org/data/2.5/'
+	url: 'https://api.openweathermap.org/data/2.5/'
 };
 
 function App() {
 
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  // Function to ping API (GET request) from user query
+  const search = evt => {
+	if (evt.key === 'Enter') {
+		// Get weather from API with fetch
+		fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+		// Get JSON promise from API response
+		.then(response => response.json())
+		// Pass JSON promise to another promise
+		.then(result => 
+			setWeather(result));
+			//Reset query field after search submission
+			setQuery('');
+			console.log(weather);
+	}
+  };
+
+  // Function build date into string format
   const dateBuilder = (d) => {
 	let months = ['January', 'February', 'March', 'April', 
 				'May', 'June', 'July', 'August', 'September',
@@ -29,14 +49,27 @@ function App() {
 				<input 
 					type="text" 
 					className="search-bar" 
-					placeHolder="Search..."
+					placeholder="Search..."
+					onChange={e => setQuery(e.target.value)}
+					value={query}
+					onKeyPress={search}
 				/>
 			</div>
-			<div className="location-box">
-				<div className="location">
-					New York City, US
+			<div className="weather-info">
+				<div className="location-box">
+					<div className="location">
+						New York City, US
+					</div>
+					<div className="date">{dateBuilder(new Date())}</div>
 				</div>
-				<div className="date">{dateBuilder(new Date())}</div>
+				<div className="weather-box">
+					<div className="temperature">
+						15°C
+					</div>
+					<div className="weather">
+						Sunny
+					</div>
+				</div>
 			</div>
 		</main>
     </div>
